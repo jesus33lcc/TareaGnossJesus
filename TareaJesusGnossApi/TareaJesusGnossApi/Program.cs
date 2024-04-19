996c0422e0971001af9d6266a8ef72032fc9571e
jesus33lcc <jesus33lcc@gmail.com>
2024-04-19 18:20:16 +0200

    Carga Género de Prueba

--- TareaJesusGnossApi/TareaJesusGnossApi/Program.cs
+++ TareaJesusGnossApi/TareaJesusGnossApi/Program.cs
@@ -1,8 +1,35 @@
-﻿
+﻿using Gnoss.ApiWrapper;
+using Gnoss.ApiWrapper.ApiModel;
+using Gnoss.ApiWrapper.Model;
+using System;
+using TgenerojlOntology;
+using TpeliculajlOntology;
+using TpersonajlOntology;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        string pathOAuth = @"Config\oAuth_demo-gnoss-akademia-20-20.config";
+        ResourceApi mResourceApi = new ResourceApi(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, pathOAuth));
+        CommunityApi mCommunityApi = new CommunityApi(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, pathOAuth));
+        ThesaurusApi mThesaurusApi = new ThesaurusApi(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, pathOAuth));
+        UserApi mUserApi = new UserApi(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, pathOAuth));
+
+        //Cargar un Género de Prueba[Commit: "Carga Género de Prueba"]
+
+        mResourceApi.ChangeOntology("tgenerojl.owl");
+        string identificador = Guid.NewGuid().ToString();
+        Genre generoPrueba = new(identificador);
+        generoPrueba.Schema_name = "Género de Prueba";
+        SecondaryResource generoSR = generoPrueba.ToGnossApiResource(mResourceApi, $"Genre_{identificador}");
+        try
+        {
+            mResourceApi.LoadSecondaryResource(generoSR);
+        }
+        catch (Exception)
+        {
+            mResourceApi.Log.Error($"Exception -> Error en cargar el genero");
+        }
     }
 }

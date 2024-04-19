a22f7df7ae43d4ca670b7e22b8339b9b8c73827e
jesus33lcc <jesus33lcc@gmail.com>
2024-04-19 18:28:31 +0200

    Modificación persona de Prueba

--- TareaJesusGnossApi/TareaJesusGnossApi/Program.cs
+++ TareaJesusGnossApi/TareaJesusGnossApi/Program.cs
@@ -34,10 +34,35 @@ internal class Program
 
         //Cargar una Persona de Prueba  [Commit: "Carga Persona de Prueba"]
 
-        mResourceApi.ChangeOntology("tpersonajl.owl");
-        Person personaPrueba = new Person();
-        personaPrueba.Schema_name = "Persona de Prueba";
-        ComplexOntologyResource resorceLoad = personaPrueba.ToGnossApiResource(mResourceApi, new List<string>() { "cine" }, Guid.NewGuid(), Guid.NewGuid());
-        mResourceApi.LoadComplexSemanticResource(resorceLoad);
+        //mResourceApi.ChangeOntology("tpersonajl.owl");
+        //Person personaPrueba = new Person();
+        //personaPrueba.Schema_name = "Persona de Prueba";
+        //ComplexOntologyResource resorceLoad = personaPrueba.ToGnossApiResource(mResourceApi, new List<string>() { "cine" }, Guid.NewGuid(), Guid.NewGuid());
+        //mResourceApi.LoadComplexSemanticResource(resorceLoad);
+
+        //Obtener los datos de la Persona de Prueba y Modificar el nombre de la Persona de Prueba [Commit:"Modificación persona de Prueba"]
+
+        string uri = "";
+        string pOntology = "tpersonajl";
+        string select = string.Empty, where = string.Empty;
+        select += $@"SELECT DISTINCT ?s";
+        where += $@" WHERE {{ ";
+        where += $@"OPTIONAL{{?s ?p 'Persona de Prueba'.}}";
+        where += $@"}}";
+        SparqlObject resultadoQuery = mResourceApi.VirtuosoQuery(select, where, pOntology);
+        if (resultadoQuery != null && resultadoQuery.results != null && resultadoQuery.results.bindings != null && resultadoQuery.results.bindings.Count > 0 && resultadoQuery.results.bindings.FirstOrDefault()?.Keys.Count > 0)
+        {
+            foreach (var item in resultadoQuery.results.bindings)
+            {
+                uri = item["s"].value;
+            }
+        }
+        string[] partes = uri.Split('/', '_');
+        string resourceId = partes[5];
+        string articleID = partes[6];
+        Person personaActor1Modificado = new Person();
+        personaActor1Modificado.Schema_name = "Persona de Prueba Modificado";
+        mResourceApi.ModifyComplexOntologyResource(personaActor1Modificado.ToGnossApiResource(mResourceApi, new List<string>() { "cine" }, new Guid(resourceId), new Guid(articleID)), false, true);
+
     }
 }
